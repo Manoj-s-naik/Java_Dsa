c772e4636863c187bcd68207a56054fd2342947a
Manoj Naik <manoj.naik.4work@gmail.com>
2025-05-22 22:38:53 +0530

    Learning about java basics

---
+++ Basics/JavaBasic.java
@@ -0,0 +1,87 @@
+// public class JavaBasic {
+//     public static void main(String[] args) {
+//         System.out.println("hello java");
+//         System.out.println("hello java");
+//         System.out.println("hello java");
+//         System.out.println("hello java");
+//         System.out.println("hello java");
+//     }
+// }
+
+// public class JavaBasic {
+//     public static void main (String[] args){
+//         System.out.println("****");
+//         System.out.println(" ***");
+//         System.out.println("  **");
+//         System.out.println("   *");
+//     }
+// }
+
+// public class JavaBasic {
+//     public static void main(String[] args){
+//         int a = 10;
+//         int b = 5;
+//         int result = a+b;
+//         System.out.println("result ="+result);
+
+//         if(a>b){
+//             System.out.println("a is greater");
+//         } else {
+//             System.out.println("b is greater");
+//         }
+
+//         if(a>b){
+//             System.out.println("a is greater");
+//         }
+//     }
+// }
+
+
+// import java.util.*;
+// public class JavaBasic {
+//     public static void main(String[] args){
+       
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter your name ?");
+// String name = sc.nextLine();
+// System.out.println(name);
+//     }
+// }
+
+// import java.util.*;
+// public class JavaBasic {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("enter a number?");
+//     int a = sc.nextInt();
+//     System.out.println("enter a number?");
+//     int b = sc.nextInt();
+//     int result = a+b;
+//     System.out.println(result);
+// }
+// }
+
+// import java.util.*;
+// public class JavaBasic {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number?");
+//         int a = sc.nextInt();
+//         System.out.println("enter a number?");
+//         int b = sc.nextInt();
+// int product = a*b;
+// System.out.println(product);
+
+//     }
+// }
+
+import java.util.*;
+public class JavaBasic {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter radius ?");
+        Float radius = sc.nextFloat();
+        Float result = 3.14 * radius * radius;
+        System.out.println(result);
+    }
+}
