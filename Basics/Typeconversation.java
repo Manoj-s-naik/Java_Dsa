e2c1d0d5a8c57ebbc8e6f6e30745352b86a62c9f
Manoj Naik <manoj.naik.4work@gmail.com>
2025-05-23 06:47:16 +0530

    Learned about Typecasting in java

---
+++ Basics/Typeconversation.java
@@ -0,0 +1,16 @@
+public class Typeconversation {
+    public static void main(String[] args){
+        // int a = 10;
+        // long b =a;
+        // char ch = 'B';
+        // int c = ch;
+        // System.out.println(c);
+
+        // float a = 12.32f;
+        // int b = (int) a;
+
+        int a = 10;
+        float b = a;
+        System.out.println(b);
+    }
+}
