3c8e7c1aea5b5f786c78f2d70e263825052b3128
Manoj Naik <manoj.naik.4work@gmail.com>
2025-05-23 19:39:50 +0530

    Variables and data types question solved.

---
+++ Basics/questions/PracticeQuestion.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class PracticeQuestion {
+    public static void main(String[] args){
+//  Question 1 : In a program, input 3 numbers: A, B andC. Youhave to output the average of
+//  these 3 numbers.
+//  (Hint : Average of N numbers is sum of those numbers divided by N)
+        // int A = 10;
+        // int B = 10;
+        // int C = 10;
+        // int average = (A+B+C)/3;
+        // System.out.println("the average ofthese 3 numbers :"+average);
+
+//          Question 2: In a program, input the side of a square. You have to output the area of the
+//  square.
+//  (Hint : area of a square is (side x side))
+
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter a side of the square?");
+// float a = sc.nextFloat();
+// float area = a*a;
+// System.out.println("the area of the square is :"+area);
+
+//  Question 3: Enter cost of 3 items from the user (using float data type)- a pencil, a pen and
+//  an eraser. You have to output the total cost of the items back to the user as their bill.
+//  (Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem)
+
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter the pencil rate?");
+// float pencil = sc.nextFloat();
+// System.out.println("enter the pen rate?");
+// float pen = sc.nextFloat();
+// System.out.println("enter the eracer rate?");
+// float eraser = sc.nextFloat();
+
+// float productCost = pencil+pen+eraser;
+
+// float gstAmmount = (productCost * 18) /100;
+
+// float totalBill = productCost + gstAmmount;
+
+
+// System.out.println("the total cost of the bill is :"+totalBill);
+
+
+
+// Question 5:(Advanced)Will the following statementgive any error in Java?int$=24
+
+// int $ =24;
+// System.out.println($);
+
+
+
+
+    }
+}
